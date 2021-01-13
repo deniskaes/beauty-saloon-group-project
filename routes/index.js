00187b0e403bc20e7f193b87821a85ceba3a3459
Esaulov Denis <deniskaes@gmail.com>
2021-01-13 20:41:13 +0300

    session created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const salt = bcrypt.genSaltSync(10);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/reg', (req, res) => {
+  res.render('reg', { layout: false });
+});
+
+router.post('/reg', async (req, res) => {
+  const { login, password } = req.body;
+  const cryptedPass = await bcrypt.hash(password, salt);
+  const admin = new User({ username: login, password: cryptedPass });
+  await admin.save();
+  res.send('registration success')
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { layout: false });
+});
+
+router.post('/login', async (req, res) => {
+  const { login, password } = req.body;
+  const user = await User.findOne({ username: login });
+  if (bcrypt.compare(password, user.password)) {
+    req.session.username = login;
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
